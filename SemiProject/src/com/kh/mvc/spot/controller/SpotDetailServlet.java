2a0de69ea952d7b70bc2729d6d4833f0bfb7bd7c
KBroJ <kbroj9210@gmail.com>
2021-12-24 16:49:22 +0900

    21.12.24_고유번호값으로 상세페이지의 값을 가져오기

--- SemiProject/src/com/kh/mvc/spot/controller/SpotDetailServlet.java
+++ SemiProject/src/com/kh/mvc/spot/controller/SpotDetailServlet.java
@@ -1,14 +1,16 @@
 package com.kh.mvc.spot.controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.kh.mvc.member.model.service.MemberService;
 import com.kh.mvc.spot.model.service.SpotDetailService;
+import com.kh.mvc.spot.model.vo.SpotDetailVo;
 
 
 @WebServlet("/spot/spotDetail")
@@ -23,6 +25,20 @@ public class SpotDetailServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// 사용자가 준 no값(파란글)을 숫자로 읽어오기
+		int no = Integer.parseInt(request.getParameter("no"));
+		
+		System.out.println("NO : " + no);
+		 
+		// Ctr+1로 SpotDetailService(서비스)에 findByNo() 메소드 생성 하고 리턴되는 값을
+    	// SpotDetailVo(vo) 클래스인 변수 detailVo에 저장		
+		SpotDetailVo detailVo = service.findByNo(no);
+		
+		System.out.println(detailVo);
+		
+		// request객체에 저장
+		request.setAttribute("detailVo", detailVo);		
 		request.getRequestDispatcher("/views/spot/spotDetail.jsp").forward(request, response);
 	}
 
