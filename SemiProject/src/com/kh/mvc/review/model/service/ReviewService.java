f45d8226d6b03dff65e8cdc607b108a7f6376129
kimyiyoung <89180893+kimyiyoung@users.noreply.github.com>
2021-12-26 18:51:46 +0900

    Update ReviewService.java
    리뷰 수정

--- SemiProject/src/com/kh/mvc/review/model/service/ReviewService.java
+++ SemiProject/src/com/kh/mvc/review/model/service/ReviewService.java
@@ -1,9 +1,10 @@
 package com.kh.mvc.review.model.service;
 
-import static com.kh.mvc.common.jdbc.JDBCTemplate.close;
+import static com.kh.mvc.common.jdbc.JDBCTemplate.*;
 import static com.kh.mvc.common.jdbc.JDBCTemplate.getConnection;
 
 import java.sql.Connection;
+import java.util.List;
 
 import com.kh.mvc.review.model.dao.ReviewDao;
 import com.kh.mvc.review.model.vo.Review;
@@ -19,20 +20,44 @@ public class ReviewService {
 		
 		review = dao.findReviewByNo(connection, no);
 		
-		// 게시글 조회수를 증가시키는 로직
-//		if(review != null && !hasRead) {
-//			int result = dao.updateReadCount(connection, review);
-//			
-//			if(result > 0) {
-//				commit(connection);
-//			} else {
-//				rollback(connection);
-//			}
-//		}
+		// 게시글 조회수 증가
+		if(review != null && !hasRead) {
+			int result = dao.updateReadCount(connection, review);
+			
+			if(result > 0) {
+				commit(connection);
+			} else {
+				rollback(connection);
+			}
+		}
 		
 		close(connection);		
 		
 		return review;
 	}
+
+
+	public int save(Review review) {
+		int result = 0;
+		Connection connection = getConnection();
+		
+		if (review.getRv_no() != 0 ) {
+			result = dao.updateReview(connection, review);
+		} else {
+			result = dao.insertBoard(connection, review);
+		}
+		
+		if(result > 0) {
+			commit(connection);
+		} else {
+			rollback(connection);
+		}
+		
+		close(connection);		
+		
+		return result;
+	}	
+	
+
 
 }
